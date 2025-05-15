567c87c420c553da6edd02a5aed0e727b9558190
VisPatrick <pksantos1003@gmail.com>
2025-05-15 19:06:14 -0300

    Exceções

---
+++ aula11/exemplo1.py
@@ -0,0 +1,21 @@
+#  ######### Exceções
+# nome = 'João'
+# print(nome)
+
+# idade = int(input('\nDigite a idade: '))    #ValueError 
+# print(idade)
+
+# n1 = int(input('\nInforme o número: '))
+# n2 = input('Informe o número: ')
+
+# resultado = n1 + n2  # TypeErro
+# print(resultado)
+
+# n1 = int(input('\nInforme o número: '))
+# n2 = int(input('Informe o número: '))
+
+# div = n1 / n2
+# print(div)
+
+list_num = ['Maria', 'João', 'Pedro']
+print(list_num[3])  # IndexError
